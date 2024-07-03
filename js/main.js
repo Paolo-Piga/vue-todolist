a3d0fdc0b23fd00b9459669d271b83532413d2ba
DecimTheJudge <paolo1piga@gmail.com>
2024-07-03 16:00:13 +0200

    inizio app su js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 const app = Vue.createApp({
-    
+    data(){
+        return{
+            activities: [
+                {text: `andare al cinema`, done: false },
+                {text: `comprare il latte`, done: false },
+            ],
+            newActiv: ``,
+        
+        }
+    },
+    methods: {
+        aggiungi() {
+            this.activities.push({ text: this.newActiv, done: flase });
+            this.newActiv = ``;
+        }
+    }
 })
